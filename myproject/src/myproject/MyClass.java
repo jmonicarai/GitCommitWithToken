d45b9c28626d26da1d58e486e953dd0c4393ceed
jmonicarai <jmonicarai8@gmail.com>
2021-12-21 10:29:47 +0530

    Next update

---
+++ myproject/src/myproject/MyClass.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class RemoveDuplicancy
+{
+public static void main(String[] args)
+{
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter the array elements:");
+int[] a = new int[10];
+for(int i=0;i<a.length-1;i++)
+{
+a[i] =sc.nextInt();
+}
+int j=0;
+
+ for(int i=0;i<a.length-1;i++)
+{
+if(a[i]!=a[i+1])
+{
+a[j]=a[i];
+j++;
+}
+}
+a[j]=a[a.length-1];
+
+ System.out.println("The array elements:");
+for(int i=0;i<j;i++)
+{
+System.out.println(a[i]+" ");
+}
+}
+}
